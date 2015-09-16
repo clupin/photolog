6158addc4a198b51336b44a80923d89c46da78ef
clupin <cristian.espinozal@usach.cl>
2015-09-16 10:14:41 -0300

    ahora se permite que el usuario edite los datos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :nombre, :apellido, :email, :password, :password_confirmation)}
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username,:nombre, :apellido, :email, :password, :current_password) }
 	end
 
 	def authenticate_normal_user!
