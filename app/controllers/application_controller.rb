13afc72513764f043ef75cb4762cf4b3190fff17
clupin <cristian.espinozal@usach.cl>
2015-09-16 09:30:03 -0300

    agregado q pida en el registro (y guarde) username, nombre y apellido, ademas de lo que ya se pedia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
+	before_action :configure_permitted_parameters, if: :devise_controller?
 	protect_from_forgery with: :exception
-
+	
 	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :nombre, :apellido, :email, :password, :password_confirmation)}
+	end
 
 	def authenticate_normal_user!
 		redirect_to root_path user_signed_in? && current_user.is_normal_user?
